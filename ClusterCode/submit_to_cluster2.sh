ce04019ed42aa4112983bdc657a5fb6f0d2d13cc
andreasmorr <119334644+andreasmorr@users.noreply.github.com>
2023-09-22 10:46:09 +0200

    prepare for short run

---
+++ ClusterCode/submit_to_cluster2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --qos=short
+#SBATCH --job-name=estimator_aucs
+#SBATCH --output=%x-%j.out 
+#SBATCH --error=%x-%j.err
+#SBATCH --account=tipes
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --workdir=/home/andreasm/RedNoiseEstimatorComparison/ClusterCode/
+
+echo "------------------------------------------------------------"
+echo "SLURM JOB ID: $SLURM_JOBID"
+echo "$SLURM_NTASKS tasks"
+echo "------------------------------------------------------------"
+
+module load anaconda
+source activate env_andreasm
+
+export I_MPI_PMI_LIBRARY=/p/system/slurm/lib/libpmi.so
+
+srun -n 1 python run_part.py
+
+
+
+
+
