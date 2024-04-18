9a021a2ea227502e3ccc6ec1005208facb90d389
andreasmorr <119334644+andreasmorr@users.noreply.github.com>
2024-04-18 16:42:31 +0200

    Green Sahara data preprocessing

--- ClusterCode/construct_data_publication.py
+++ ClusterCode/construct_data_publication.py
@@ -4,39 +4,43 @@ import pandas as pd
 import numpy as np
 import math
 
-descriptions = ["precip_mm_srf", "fracPFTs_mm_srf", "sm_mm_soil", "temp_mm_1_5m"]
-for i in [0,1]:
-    data = Dataset("Holocene data/xokm." + descriptions[i] + ".monthly.nc")
-    print(data.variables)
+cells = [[26,-3],[26,-2],[26,-1],[26,0],[26,1],[27,-4],[27,-3],[27,-2],[27,-1],[27,0],[27,1],[28,-4],[28,-3],[28,-2],[28,-1],[28,0],[28,1]]
+
+
+month_ind = [float(j) / 12 for j in range(-120000, 0)]
+year_ind = range(-10000,0)
+season = range(12)
+
+
+for [k, l] in cells:
+    df = pd.DataFrame(0,index = year_ind, columns = ["p","v"])
+
+    description = "precip_mm_srf"
+    data = Dataset("Holocene data/xokm." + description + ".monthly.nc")
     lats = data.variables['latitude'][:]
     lons = data.variables['longitude'][:]
-    month_ind = [float(j) / 12 for j in range(-120000, 0)]
-    year_ind = range(-10000,0)
-    season = range(12)
-    time_values = np.zeros(len(month_ind))
-    for k in range(26,29):
-        for l in range(-4,2):
-            if not (k == 26 and l == -4):
-                mask_count = 0
-                if i == 0:
-                    this_data = data.variables[descriptions[i]][:, 0, k, l]
-                else:
-                    this_data = data.variables[descriptions[i]][:, k, l]
-                for j in range(this_data.size):
-                    if np.ma.getmask(this_data)[j]:
-                        this_data[j] = this_data[j - 12]
-                        mask_count += 1
-                time_values += this_data.data
-                if i==0:
-                    pd.Series(np.array([sum([this_data.data[12 * i + j] for j in season])/len(season) for i in range(math.floor(len(this_data.data)/12))]), year_ind).to_csv("Processed/Cell Data/seasonal_data_" + descriptions[i] + "lat" + str(k) + "lon" + str(l))
-                else:
-                    pd.Series(1-np.array([sum([this_data.data[12 * i + j] for j in season]) / len(season) for i in range(math.floor(len(this_data.data) / 12))]), year_ind).to_csv("Processed/Cell Data/seasonal_data_" + descriptions[i] + "lat" + str(k) + "lon" + str(l))
-                print(mask_count)
-    time_values = time_values/17
-    if i == 1:
-        time_values = 1 - time_values #noprecip
-    time_series = pd.Series(time_values, month_ind)
-    time_series.to_csv("Processed/west_data_" + descriptions[i])
-    seasonal_values = [sum([time_values[12*i + j] for j in season])/len(season) for i in year_ind]
-    seasonal_series = pd.Series(seasonal_values, year_ind)
-    seasonal_series.to_csv("Processed/seasonal_west_data_" + descriptions[i])
+    print("Lat: "+str(lats[k])+ ", Lon: " + str(lons[l]))
+    mask_count = 0
+    this_data = data.variables[description][:, 0, k, l]
+    for j in range(this_data.size):
+        if np.ma.getmask(this_data)[j]:
+            this_data[j] = this_data[j - 12]
+            mask_count += 1
+    df.loc[:,"p"] = np.array([sum([this_data.data[12 * i + j] for j in season])/len(season) for i in range(math.floor(len(this_data.data)/12))])
+    print("Precip mask count: "+str(mask_count))
+    description = "fracPFTs_mm_srf"
+    data = Dataset("Holocene data/xokm." + description + ".monthly.nc")
+    mask_count = 0
+    this_data = data.variables[description][:, k, l]
+    for j in range(this_data.size):
+        if np.ma.getmask(this_data)[j]:
+            this_data[j] = this_data[j - 12]
+            mask_count += 1
+    df.loc[:,"v"] = 1-np.array([sum([this_data.data[12 * i + j] for j in season]) / len(season) for i in range(math.floor(len(this_data.data) / 12))])
+    print("Veg mask count: "+str(mask_count))
+    df.to_csv("Processed/Cell Data/lat" + str(k) + "_lon" + str(l))
+
+pd.Series(lats).to_csv("Processed/lats")
+pd.Series(lons).to_csv("Processed/lons")
+
+
